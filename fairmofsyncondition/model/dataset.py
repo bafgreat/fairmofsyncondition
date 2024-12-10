22c4ffa31eafc7b779654938409e8e063f047c97
bafgreat <75862753+bafgreat@users.noreply.github.com>
2024-12-10 18:56:24 +0100

    new data handler

---
+++ fairmofsyncondition/model/dataset.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python
+from __future__ import print_function
+__author__ = "Dr. Dinga Wonanke"
+__status__ = "production"
+
+##############################################################################
+# fairmofsyncondition is a machine learning package for predicting the        #
+# synthesis condition of the crystal structures of MOFs. It is also intended  #
+# for predicting all MOFs the can be generated from a given set of conditions #
+# In addition the package also predicts the stability of MOFs, compute their  #
+# their PXRD and crystallite sizes. This package is part of our effort to     #
+# to accelerate the discovery and optimization of the synthesises of novel    #
+# high performing MOFs. This package is being developed by Dr Dinga Wonanke   #
+# as part of hos MSCA post doctoral fellowship at TU Dresden.                 #
+#                                                                             #
+###############################################################################
+
+
+from fairmofsyncondition.read_write import coords_library, filetyper
