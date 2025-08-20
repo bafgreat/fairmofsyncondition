69b69aa64f12b256bbe363d537aa5f3a2bf61839
AntonioLonga <antonio.longa@gmail.com>
2025-08-20 14:11:15 +0200

    add ml

---
+++ ML/README.md
@@ -0,0 +1,12 @@
+# Machine Learning use cases
+
+
+
+Here you will find the code to predict the metal salts of a given MOF with different input.
+
+
+| File name           | Scherrer | Microstrain | OMS | Atomic No. |
+|-------------------- |----------|-------------|-----|------------|
+| Ex1_using edgeAttr  |  ✗       | ✗           | ✗   | ✗          |
+| Ex2_using edgeAttr  |  ✓       | ✗           | ✗   | ✓          |
+
---
+++ ML/utils.py
@@ -0,0 +1,52 @@
+
+import random
+import numpy as np
+import torch
+from torch_geometric.data import Data
+
+
+
+from tqdm import tqdm
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def fix_target_shapes(data_list,attr=None):
+    cleaned_data = []
+    for data in data_list:
+        target = getattr(data, attr, None)
+        if target is not None and target.dim() == 2 and target.shape[0] > 1:
+            # Prendi solo la prima riga
+            setattr(data, attr, target[1].unsqueeze(0))  # Shape diventa [1, y]
+        cleaned_data.append(data)
+    return cleaned_data
+
+def remove_unused_onehot_columns(data_list, target_field="metal_salts"):
+    # Controllo esistenza campo
+    if not hasattr(data_list[0], target_field):
+        raise ValueError(f"Il campo '{target_field}' non esiste nei dati.")
+    
+    # Trova colonne non nulle
+    field_matrix = getattr(data_list[0], target_field)
+    used_columns = torch.zeros(field_matrix.shape[1], dtype=torch.bool)
+
+    # Accumula colonne usate
+    for data in data_list:
+        matrix = getattr(data, target_field)
+        used_columns |= matrix.bool().squeeze(0)
+
+    # Filtra ogni grafo
+    for data in data_list:
+        matrix = getattr(data, target_field)
+        filtered = matrix[:, used_columns]
+        setattr(data, target_field, filtered)
+
+    return data_list
+
+
